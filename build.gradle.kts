632325a76d7a6ee85b09f09257c27994fd6179fc
Tech <debitcardzpaypal@gmail.com>
2023-11-25 10:29:11 -0500

    fix: kotlin not being shaded

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-
     compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
 }
 
--- development/build.gradle.kts
+++ development/build.gradle.kts
@@ -2,6 +2,7 @@ import xyz.jpenilla.runpaper.task.RunServer
 
 plugins {
     kotlin("jvm") version "1.9.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 
     id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
     id("xyz.jpenilla.run-paper") version "2.1.0"
@@ -19,6 +20,7 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
 
+//    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
     implementation(rootProject)
 }
 
