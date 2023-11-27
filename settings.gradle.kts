59f6686762a51a54ae7b62c1e28c7cfa6aad1e3a
Tech <debitcardzpaypal@gmail.com>
2023-11-27 16:45:42 -0500

    fix: further exclude

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,8 @@ rootProject.name = "mc-chestui-plus"
 
 if(System.getenv("JITPACK") != null) {
     include("development")
-}
 
-startParameter.excludedTaskNames += ":development:build"
+    startParameter.excludedTaskNames += ":development:build"
+    startParameter.excludedTaskNames += ":development:compileJava"
+    startParameter.excludedTaskNames += ":development:compileKotlin"
+}
