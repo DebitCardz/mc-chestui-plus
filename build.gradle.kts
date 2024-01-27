3398123775cf8a4f370304e02b59cecb2b7a42b9
DebitCardz <debitcardzpaypal@gmail.com>
2024-01-26 20:14:13 -0500

    misc: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val githubActor = project.findProperty("gpr.user") as String? ?: System.getenv("
 val githubToken = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
 
 group = "me.tech"
-version = "1.4.5"
+version = "1.4.6"
 
 repositories {
     mavenCentral()
