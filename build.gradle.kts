b860c2c2873057188b8275745134f2f1ebd70fb4
tarna <46247728+tarna@users.noreply.github.com>
2025-08-25 14:49:06 -0400

    feat: update to 1.21.8 (#30)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,11 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 java {
